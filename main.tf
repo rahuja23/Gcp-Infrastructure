80691b299ee74fff91d4244dd6427b4105cc3bea
rahuja23 <51020974+rahuja23@users.noreply.github.com>
2023-09-01 22:36:02 +0200

    Changed bucket names

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "google_storage_bucket" "bucket" {
-  name     = "Blogpost-ELT-test-bucket1"
+  name     = "blogpost-elt-test-bucket1"
   location = var.region
 }
 
 resource "google_storage_bucket" "gcs_bucket" {
-  name     = "Blogpost-ELT-test-bucket2"
+  name     = "blogpost-elt-test-bucket2"
   location = var.region
 }
