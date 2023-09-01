554b6d441569eba1e6eea0aef732e7827460f8b2
rahuja23 <51020974+rahuja23@users.noreply.github.com>
2023-09-01 22:09:31 +0200

    Removed cred path

--- providers.tf
+++ providers.tf
@@ -1,7 +1,6 @@
 provider "google" {
-  credentials = file("/Users/rachitahuja/Desktop/Gcp-Infrastructure/blogpost-demo-a108282c2d25.json")
-  project = var.project_id
-  region  = var.region
+  project     = var.project_id
+  region      = var.region
 }
 
 terraform {
