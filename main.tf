9447a776c0c6def3839bd64d2ace3851582169a8
rahuja23 <51020974+rahuja23@users.noreply.github.com>
2023-09-01 22:40:57 +0200

    Adding required infrastructure for blogpost

--- main.tf
+++ main.tf
@@ -1,9 +1,42 @@
-resource "google_storage_bucket" "bucket" {
-  name     = "blogpost-elt-test-bucket1"
-  location = var.region
+# Data Lake Bucket
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/storage_bucket
+locals {
+  data_lake_bucket = "blogpost_data_lake"
+}
+resource "google_storage_bucket" "data-lake-bucket" {
+  name          = "${local.data_lake_bucket}" # Concatenating DL bucket & Project name for unique naming
+  location      = var.region
+
+  # Optional, but recommended settings:
+  storage_class = var.storage_class
+  uniform_bucket_level_access = true
+
+  versioning {
+    enabled     = true
+  }
+
+  lifecycle_rule {
+    action {
+      type = "Delete"
+    }
+    condition {
+      age = 30  // days
+    }
+  }
+
+  force_destroy = true
 }
 
-resource "google_storage_bucket" "gcs_bucket" {
-  name     = "blogpost-elt-test-bucket2"
-  location = var.region
+# DWH
+# Ref: https://registry.terraform.io/providers/hashicorp/google/latest/docs/resources/bigquery_dataset
+resource "google_bigquery_dataset" "bp_data_raw" {
+  dataset_id = var.BQ_DATASET
+  project    = var.project_id
+  location   = var.region
+}
+
+resource "google_bigquery_dataset" "bp_data_dbt" {
+  dataset_id = var.DBT_DATASET
+  project    = var.project_id
+  location   = var.region
 }
--- variables.tf
+++ variables.tf
@@ -7,3 +7,19 @@ variable "region" {
   type    = string
   default = "europe-west2"
 }
+
+variable "BQ_DATASET" {
+  description = "BigQuery Dataset that raw data (from GCS) will be written to"
+  type = string
+  default = "bp_data_raw"
+}
+
+variable "DBT_DATASET" {
+  description = "BigQuery Dataset that transformed data (from dbt) will be written to and connected to the presentation layer"
+  type = string
+  default = "bp_data_dbt"
+}
+variable "storage_class" {
+  description = "Storage class type for your bucket. Check official docs for more info."
+  default = "STANDARD"
+}
